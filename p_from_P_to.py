5d16fb6fd804a081218c3f4b182134ba790551d9
Rea <uspzg@student.kit.edu>
2020-11-21 13:16:06 +0100

    update

--- p_from_P_to.py
+++ p_from_P_to.py
@@ -25,7 +25,9 @@ ax = plt.subplot(111)
 rects1 = ax.bar(ind-0.2, k, width=0.2, color='b', align='center')
 rects2 = ax.bar(ind, l, width=0.2, color='g', align='center')
 rects3 = ax.bar(ind+0.2, m, width=0.2, color='r', align='center')
-labels = ('K_1-2', 'K_2-3', 'K_3-4', 'K_4-5', 'K_5-6', 'K_2-10', 'K_10-9', 'K_9-8', 'K_8-7', 'K_5-7', 'K_10-11', 'K_11-12', 'K_12-13', 'K_12-14', 'K_7-13')
+labels = ('K_1-2', 'K_2-3', 'K_3-4', 'K_4-5', 'K_5-6', 'K_2-10', 
+          'K_10-9', 'K_9-8', 'K_8-7', 'K_5-7', 'K_10-11', 'K_11-12', 
+          'K_12-13', 'K_12-14', 'K_7-13')
 plt.xticks(np.arange(15), labels, rotation=45)
 
 plt.autoscale(tight=True)
