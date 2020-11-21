112f1a76f77f2f73343d1b52cd4e93338c84e5b1
Rea <uspzg@student.kit.edu>
2020-11-21 13:57:25 +0100

    u

--- fehlerabweichung_druck_unterschiedl_k.py
+++ fehlerabweichung_druck_unterschiedl_k.py
@@ -25,7 +25,8 @@ rects1 = ax.bar(ind-0.2, k, width=0.2, color='b', align='center')
 rects2 = ax.bar(ind, l, width=0.2, color='g', align='center')
 rects3 = ax.bar(ind+0.2, f, width=0.2, color='r', align='center')
 
-labels = ('K1', 'K2', 'K3', 'K4', 'K5', 'K6', 'K7', 'K8', 'K9', 'K10', 'K11', 'K12', 'K13', 'K14')
+labels = ('K1', 'K2', 'K3', 'K4', 'K5', 'K6', 'K7', 'K8', 'K9', 'K10', 
+          'K11', 'K12', 'K13', 'K14')
 
 plt.xticks(np.arange(15), labels, rotation=45)
 plt.autoscale(tight=True)
@@ -35,6 +36,8 @@ plt.xlabel('Knoten')
 plt.ylabel('Druck/bar')
 
 
-plt.legend( (rects1[0], rects2[0], rects3[0]), ('k = 1mm', 'k = 0.01mm', 'Fehlerabweichung'), bbox_to_anchor=(1.5, 1), loc='upper right' )
+plt.legend( (rects1[0], rects2[0], rects3[0]), 
+           ('k = 1mm', 'k = 0.01mm', 'Fehlerabweichung'), 
+           bbox_to_anchor=(1.5, 1), loc='upper right' )
 
 plt.show()
